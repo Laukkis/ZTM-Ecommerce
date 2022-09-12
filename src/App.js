a734a49dfa0faefe662b9157ba6a45928c0d75f7
Tatu Laukkanen <tatu.laukkanen@tietoevry.com>
2022-09-12 15:07:18 +0300

    Added React Routing and navigation component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import './components/directory/directory'
-import Directory from './components/directory/directory';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home'
+import Navigation from './routes/navigation/navigation';
+
+
+
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
   return (
-    <Directory categories={categories} />
-  );
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
